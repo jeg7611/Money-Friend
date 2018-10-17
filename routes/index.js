d098fd90e4049b7e4ae36f35fbebd4fcba63449c
Jose Eyber Garcia - Nexos <josee.garcia@carvajal.com>
2018-10-17 11:10:20 -0500

    Chain

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 
-var express = require('express');
-var app = express();
+var express = require('express'),
+app     = express();
+
 var router = express.Router();
 var db = require('../queries');
 const clientesController = require('../controllers').clientes;
@@ -24,8 +25,8 @@ router.delete('/api/clients/:id', db.removeClient);
 
 module.exports = router;
 
-var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
  
 app.listen(server_port, server_ip_address, function () {
   console.log( "Listening on " + server_ip_address + ", port " + server_port )
