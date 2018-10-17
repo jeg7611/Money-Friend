f5cf14109c052e248174286a96037eb9703f9500
Jose Eyber Garcia - Nexos <josee.garcia@carvajal.com>
2018-10-17 11:22:54 -0500

    Update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -28,7 +28,6 @@ module.exports = router;
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
  
-app.listen(server_port, server_ip_address, function () {
-  console.log( "Listening on " + server_ip_address + ", port " + server_port )
-});
+    app.listen(port, ip);
+    console.log('Server running on http://%s:%s', ip, port);
 
