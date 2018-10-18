5e4e928163902b40b6cc7a9cd49531afb35bff86
Jose Eyber Garcia - Nexos <josee.garcia@carvajal.com>
2018-10-18 15:09:51 -0500

    Update routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,28 @@ var express = require('express'),
 
 var cors = require('cors');
 
+app.use(cors());
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8080');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 var router = express.Router();
 var db = require('../queries');
 const clientesController = require('../controllers').clientes;
@@ -13,7 +35,7 @@ router.get('/', function (req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
-/* Classroom Router */
+/* clientes Router */
 router.get('/api/clientes', clientesController.list);
 router.get('/api/clientes/:id', clientesController.getById);
 
@@ -23,17 +45,6 @@ router.get('/api/clients/:id', db.getSingleClient);
 router.post('/api/clients', db.createClient);
 router.put('/api/clients/:id', db.updateClient);
 router.delete('/api/clients/:id', db.removeClient);
-
-app.use(cors());
-
-app.use((req,res,next) => {
-
-    res.header("Access-Control-Allow-Origin","*");
-    res.header("Access-Control-Allow-Methods", "POST, GET");
-    res.header("Access-Control-Allow-Header","Origin, X-Requested-With, Content-Type, Accept");
-    next();
-
-});
 
 module.exports = router;
 
